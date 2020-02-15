a497560018c907e004981f358f70f1b1c52dae16
EC2 Default User <ec2-user@ip-172-31-5-204.us-west-1.compute.internal>
2020-02-15 05:18:39 +0000

    Return static php object

--- code/data.php
+++ code/data.php
@@ -1,5 +1,17 @@
 <?php
 
-echo "{\"productList\": []}";
+// Create a PHP object
+$productObj = new stdClass();
+$productObj->productList = array(new stdClass(),new stdClass());
+$productObj->productList[0]->product = "Arduino";
+$productObj->productList[0]->price = 7;
+$productObj->productList[1]->product = "Raspberry";
+$productObj->productList[1]->price = 15;
+
+// convert object to JSON string
+$productJson = json_encode($productObj);
+
+// return the string to the client
+echo $productJson;
 
 ?>
